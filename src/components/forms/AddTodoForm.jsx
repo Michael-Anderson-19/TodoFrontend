01a98e4547ff708fc20da726e9b0edb5374b39b0
Michael-Anderson-19 <michael.i.anderson@outlook.com>
2023-02-03 20:40:10 +0000

    created add todo form and style

---
+++ src/components/forms/AddTodoForm.jsx
@@ -0,0 +1,40 @@
+import Style from "./Form.module.css";
+
+const AddTodoForm = () => {
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
+  return (
+    <form className={Style.form}>
+      <div className={Style.formItem}>
+        <label htmlFor="title" type="text">
+          Title
+        </label>
+        <input type="text" id="title" name="title" />
+      </div>
+      <div className={Style.formItem}>
+        <label htmlFor="description" type="text">
+          Description
+        </label>
+        <textarea rows={5} cols={20} />
+        {/* <input type="text" id="description" name="description" /> */}
+      </div>
+      <div className={Style.formItem}>
+        <label htmlFor="description" type="textarea">
+          Priority
+        </label>
+        <select id="description" name="description">
+          <option value="" default>
+            select priority
+          </option>
+        </select>
+      </div>
+      <div className={Style.formItemButton}>
+        <button onClick={handleSubmit}>Save</button>
+      </div>
+    </form>
+  );
+};
+
+export default AddTodoForm;
